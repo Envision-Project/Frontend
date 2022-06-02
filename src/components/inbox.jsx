2eff870894bb92a948e0573bdc3627b1a38aef22
Rakshith <rakshith21mohan@gmail.com>
2022-06-02 17:19:25 +0530

    displaying tasks and modal on click

--- src/components/inbox.jsx
+++ src/components/inbox.jsx
@@ -1,8 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import '../css/inbox.scss'
 import Sidebar from './sidebar';
+import TaskModal from './taskmodal';
 
 const Inbox = () => {
+
+    const [tasks, setTasks] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [activeTask, setActiveTask] = useState({});
+    console.log(showModal);
+
+    const sampleTasks= [
+        {
+            'id' : 1,
+            'title' : 'React app',
+            'description' : 'Envision project - activity planner',
+            // 'due_date' : '2022-06-04',
+            'priority' : 2
+        },
+        {
+            'id' : 2,
+            'title' : 'Clean my table',
+            'description' : 'Envision project - activity planner',
+            'due_date' : '2022-06-04',
+            'priority' : 2
+        },
+        {
+            'id' : 3,
+            'title' : 'Water the plants',
+            'description' : 'Envision project - activity planner',
+            'due_date' : '2022-06-04',
+            'priority' : 2
+        }
+    ]
+
+    useEffect(() => {
+        setTasks(sampleTasks);
+        console.log(tasks);
+    }, [])
+
+    function openModal(task_id) {
+        // document.getElementById('mymodal').classList.add('show')
+        setShowModal(true);
+        console.log(tasks[task_id]);
+        setActiveTask(tasks[task_id]);
+    }
+
+    function closeModal(isSave){
+        setShowModal(false);
+    }
 
     return (
 
@@ -11,6 +57,31 @@ const Inbox = () => {
                 <Sidebar selected="inbox"></Sidebar>
                 <div className='inner'>
                     <h2>Inbox</h2>
+                        <div className='todos'>
+                            {
+                                tasks.map((task, index) => {
+                                    console.log(index);
+                                    return (
+                                        <div className='todo' key={task.id} onClick={() => openModal(index)}>
+                                            <div className='check'>
+                                                <div className='hide'>
+                                                    <i className="bi bi-check"></i>
+                                                </div>
+                                            </div>
+                                            <div className='todo-title'>
+                                                {task.title}
+                                            </div>
+                                            <div className='options hide'>
+                                                <button className='options-btn'><i class="fas fa-pencil-alt me-3"></i></button>
+                                                <button className='options-btn'><i class="fas fa-trash-alt"></i></button>
+                                            </div>                                            
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <TaskModal open={showModal} task={activeTask} closeModal={() => closeModal}></TaskModal>
+                        
                 </div>
             </div>
         </div>
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -7,42 +7,42 @@ const Sidebar = (props) => {
     return (
         <div className="sidebar">
             <Link to='/inbox' className={'link ' + (props.selected == 'inbox' ? 'selected' : " ")}>
-                    <i className="bi bi-speedometer2"></i>
+                    
                 <div className={'menu-option ' + (props.selected == 'inbox' ? 'selected' : " ")}>
-                    Inbox
+                    <i class="bi bi-inbox"></i>Inbox
                 </div>
             </Link>
-            <Link to='/today' className={'link ' + (props.selected == 'today' ? 'selected' : " ")}><i class="bi bi-person"></i>
+            <Link to='/today' className={'link ' + (props.selected == 'today' ? 'selected' : " ")}>
                 <div className={'menu-option ' + (props.selected == 'today' ? 'selected' : " ")}>
-                    Today
+                <i class="bi bi-calendar"></i>Today
                 </div>
             </Link>            
-            <Link to='/upcoming' className={'link ' + (props.selected == 'upcoming' ? 'selected' : " ")}><i class="bi bi-person"></i>
+            <Link to='/upcoming' className={'link ' + (props.selected == 'upcoming' ? 'selected' : " ")}>
                 <div className={'menu-option ' + (props.selected == 'upcoming' ? 'selected' : " ")}>
-                    Upcoming
+                    <i class="bi bi-card-list"></i>Upcoming
                 </div>
             </Link>     
-            <Link to='/filterslabels' className={'link ' + (props.selected == 'filters-labels' ? 'selected' : " ")}><i class="bi bi-person"></i>
+            <Link to='/filterslabels' className={'link ' + (props.selected == 'filters-labels' ? 'selected' : " ")}>
                 <div className={'menu-option ' + (props.selected == 'filters-labels' ? 'selected' : " ")}>
-                    Filters & Labels
+                    <i class="bi bi-filter"></i>Filters & Labels
                 </div>
             </Link>      
 
             <div className='projects'>  
-                <h4 className='projects-header'>Projects</h4>
-                <Link to='/personal' className={'link ' + (props.selected == 'personal' ? 'selected' : " ")}><i class="bi bi-plus-lg"></i>
+                <h5 className='projects-header'>Projects</h5>
+                <Link to='/personal' className={'link ' + (props.selected == 'personal' ? 'selected' : " ")}>
                     <div className={'menu-option ' + (props.selected == 'personal' ? 'selected' : " ")}>
-                        Personal
+                        <i class="bi bi-person"></i>Personal
                     </div>  
                 </Link>
-                <Link to='/shopping' className={'link ' + (props.selected == 'shopping' ? 'selected' : " ")}><i class="bi bi-person"></i>
+                <Link to='/shopping' className={'link ' + (props.selected == 'shopping' ? 'selected' : " ")}>
                     <div className={'menu-option ' + (props.selected == 'shopping' ? 'selected' : " ")}>
-                        Shopping
+                    <i class="bi bi-cart"></i>Shopping
                     </div>
                 </Link>      
-                <Link to='/work' className={'link ' + (props.selected == 'work' ? 'selected' : " ")}><i class="bi bi-person"></i>
+                <Link to='/work' className={'link ' + (props.selected == 'work' ? 'selected' : " ")}>
                 <div className={'menu-option ' + (props.selected == 'work' ? 'selected' : " ")}>
-                    Work
+                    <i class="bi bi-person"></i>Work
                 </div>
             </Link>      
             </div>         
